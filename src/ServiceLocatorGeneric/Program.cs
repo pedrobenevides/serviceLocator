9e1f895dbdb894e9628a7680f73de0d94884d73f
pedrobenevides86 <pedrobenevidesb@gmail.com>
2015-08-29 11:01:00 -0300

    Criacao do projeto generico

--- src/ServiceLocatorGeneric/Program.cs
+++ src/ServiceLocatorGeneric/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace ServiceLocatorGeneric
+﻿using System;
+using ServiceLocatorGeneric.Interfaces;
+
+namespace ServiceLocatorGeneric
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var serviceLocator = new ServiceLocator();
+            
+            var serviceA = serviceLocator.GetService<IServiceA>();
+            var serviceB = serviceLocator.GetService<IServiceB>();
+
+            serviceA.Execute();
+            serviceB.Execute();
+
+            Console.ReadKey();
         }
     }
 }
